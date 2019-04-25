96bf054302d3523470262f9747dad02a10784e82
Edgar48 <32237520+Edgar48@users.noreply.github.com>
2019-04-25 17:29:36 +0200

    Beispiel auf Seite 58 implementiert

--- app/src/main/java/com/example/myapplication33/MainActivity.java
+++ app/src/main/java/com/example/myapplication33/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.myapplication33;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+/* weiter auf Seite 57*/
+    @Override
+    public void onClick(View v) {
+        EditText etZaehler = findViewById(R.id.zaehler);
+        EditText etNenner = findViewById(R.id.nenner);
+        int z = Integer.parseInt(etZaehler.getText().toString());
+        int n = Integer.parseInt(etNenner.getText().toString());
+        if (z*n != 0) {
+            int rest;
+            int ggt = Math.abs(z);
+            int divisor = Math.abs(n);
+            do { rest = ggt % divisor;
+            ggt = divisor; divisor = rest;
+            }  while (rest > 0);
+            z /= ggt;
+            n /= ggt; }
+        etZaehler.setText(Integer.toString(z));
+        etNenner.setText(Integer.toString(n));
     }
 }
 /*test33-2*/
